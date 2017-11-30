7bbe3ad50403e5118edcf2d15ca27c08039a0576
reyounglee <reyounglee@live.cn>
2017-11-30 18:11:26 +0800

    done

---
+++ index.js
@@ -0,0 +1,47 @@
+let scoreMap = [
+	1,
+	2,
+	2.5,
+	3, 3,
+	3.5, 3.5, 3.5, 3.5,
+	4, 4, 4,
+	4.5, 4.5, 4.5,
+	5, 5, 5, 5,
+	5.5, 5.5, 5.5,
+	6, 6, 6, 6,
+	6.5, 6.5, 6.5,
+	7, 7, 7,
+	7.5, 7.5,
+	8, 8,
+	8.5, 8.5,
+	9, 9
+]
+
+let [result, totalResult] = document.querySelectorAll('p'),
+	[calcItem, calcAll] = document.querySelectorAll('button'),
+	[amountL, amountR] = document.querySelectorAll('.amount input'),
+	itemScores = document.querySelectorAll('.score input')
+
+calcItem.onclick = () => {
+	let listeningAmount = scoreMap[amountL.value - 1] || 0,
+		readingAmount = scoreMap[amountR.value - 1] || 0 
+	result.innerHTML = `Listening score ${listeningAmount}, reading score ${readingAmount}`
+}
+
+calcAll.onclick = () => {
+	let total = 0
+	for (let item of itemScores) {
+		total += +item.value
+	}
+	total /= 4;
+	let int = parseInt(total),
+		dec = total - int
+	if (dec < 0.25){
+		total = int
+	} else if (dec < 0.75) {
+		total = int + 0.5
+	} else {
+		total = int + 1
+	}
+	totalResult.innerHTML = `Total score ${total}`
+}
