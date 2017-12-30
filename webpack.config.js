01092f7dbf934e11e4f0e3a1028952c80e77ce3e
reyounglee <reyounglee@live.cn>
2017-12-30 22:23:59 +0800

    scss&js bundle done

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,39 @@
 // const webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: __dirname,
-        filename: 'prd.js'
-    },
-    devServer: {
-    },
-    module: {
-        loaders: [{
-            test: /\.js?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-                presets: ['env']
-            }
-        },{
-            test: /\.scss?$/,
-            loader: 'style-loader!css-loader!sass-loader'
-        },]
-    }
+	entry: './src/index.js',
+	output: {
+		path: __dirname,
+		filename: 'prd.js'
+	},
+	devServer: {
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				query: {
+					presets: ['env']
+				}
+			},
+			{
+				test: /\.scss?$/,
+				loader: 'style-loader!css-loader!sass-loader'
+			},
+			// {
+			// 	test: /\.css$/,
+			// 	loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+			// },
+			// {
+			// 	test: /\.scss$/,
+			// 	loader: ExtractTextPlugin.extract('style-loader', 'css-loader!scss-loader')
+			// }
+		]
+	},
+	// plugins: [
+	// 	new ExtractTextPlugin('prd.css')
+	// ]
 }
